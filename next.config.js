2a5351a6b2a7752c65803d357d2706035b7b80c0
mikebernal <mikebernal.me@gmail.com>
2020-11-30 19:08:46 +0800

    restored next.config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    images: {
+      domains: ['images.unsplash.com'],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
+    env: {
+      UNSPLASH_CLIENT_ID: "qpcqED-8j9HWAwrzwVNBYhjAK6VteN5WlRq9CxY0IaA",
+      UNSPLASH_CLIENT_SECRET: "c9LIPYCtpKaLrTRu0-HY1SoxZzUvDm3bTj9ht4-o5_I",
+      UNSPLASH_CLIENT_ID2: "MYdLLAb-tOd88gaq6yzkGKh86IzMFJqPg96x0aJWk2o",
+      UNSPLASH_CLIENT_ID3: "AgTsdyy9I-hCQRNWx-yiVzibzPZro15CZHtCzVlRdyM",
+      UNSPLASH_CLIENT_ID4: "HfYrnuq7MHi2vbUK0qhFWe7Gvad97sRKZMxhSTBuOgM",
+    },
+    
+}
